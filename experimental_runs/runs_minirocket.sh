a51e5f1a2587999794dd4b73fe0308882376ae1f
scken <37331438+scken@users.noreply.github.com>
2025-07-24 10:52:56 +0200

    Initial commit of MiniROCKET run script

--- experimental_runs/runs_minirocket.sh
+++ experimental_runs/runs_minirocket.sh
@@ -6,33 +6,38 @@ cd ..
 # define global variable for results
 MODEL="MINIROCKET"
 HDC_MODEL="HDC"_$MODEL
-RESULTS="results_comp"
+RESULTS="results/synthetic"
 
 # synthetic dataset 1
-python3 main.py --config-name=$MODEL variant=orig results_path=$RESULTS dataset=synthetic
-python3 main.py --config-name=$MODEL variant=hdc_auto results_path=$RESULTS dataset=synthetic
+python3 main.py --config-name=$MODEL variant=orig results_path=$RESULTS dataset=synthetic1
+python3 main.py --config-name=$MODEL variant=hdc_auto results_path=$RESULTS dataset=synthetic1
 
 # synthetic dataset 1 hard
-python3 main.py --config-name=$MODEL variant=orig results_path=$RESULTS dataset=synthetic_hard
-python3 main.py --config-name=$MODEL variant=hdc_auto results_path=$RESULTS dataset=synthetic_hard
+python3 main.py --config-name=$MODEL variant=orig results_path=$RESULTS dataset=synthetic1 hard_case=True
+python3 main.py --config-name=$MODEL variant=hdc_auto results_path=$RESULTS dataset=synthetic1 hard_case=True
 
 # synthetic dataset 2
 python3 main.py --config-name=$MODEL variant=orig results_path=$RESULTS dataset=synthetic2
 python3 main.py --config-name=$MODEL variant=hdc_auto results_path=$RESULTS dataset=synthetic2
 
 # synthetic dataset 2 hard
-python3 main.py --config-name=$MODEL variant=orig results_path=$RESULTS dataset=synthetic2_hard
-python3 main.py --config-name=$MODEL variant=hdc_auto results_path=$RESULTS dataset=synthetic2_hard
+python3 main.py --config-name=$MODEL variant=orig results_path=$RESULTS dataset=synthetic2 hard_case=True
+python3 main.py --config-name=$MODEL variant=hdc_auto results_path=$RESULTS dataset=synthetic2 hard_case=True
 
 ###################
 #### UCR
 ###################
+KERNEL="sinc"
+KERNEL_LIST='["'"$KERNEL"'"]'
+
+RESULTS="results/UCR/_orig_models"
 # orig. Minirocket
-python3 main.py --config-name=$MODEL variant=orig results_path=$RESULTS complete_UCR=True
+python3 main.py --config-name=$MODEL variant=orig results_path=$RESULTS complete_UCR=True kernels=$KERNEL_LIST
 
 ### HDC-Version
+RESULTS="results/UCR/_${KERNEL}"
 # all scales and kernels
-python3 main.py --config-name=$MODEL variant=hdc_oracle results_path=$RESULTS complete_UCR_NEW=True
+python3 main.py --config-name=$MODEL variant=hdc_oracle results_path=$RESULTS complete_UCR_NEW=True kernels=$KERNEL_LIST
 
 # data driven selection of scale and kernel
-python3 main.py --config-name=$MODEL variant=hdc_auto results_path=$RESULTS complete_UCR_NEW=True
+python3 main.py --config-name=$MODEL variant=hdc_auto results_path=$RESULTS complete_UCR_NEW=True kernels=$KERNEL_LIST
